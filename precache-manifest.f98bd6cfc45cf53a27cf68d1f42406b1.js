a36218fc4d2604a4dea9f0414b2c121b6137cad9
aliasghar <lionleo1374@yahoo.com>
2019-10-15 21:58:59 +0330

    Updates

---
+++ precache-manifest.f98bd6cfc45cf53a27cf68d1f42406b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16aa1fb6e4bbdea24c00ef96294f43c",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "09e4f2bab13c6d71412f",
+    "url": "/react-monsters/static/css/main.19ef684c.chunk.css"
+  },
+  {
+    "revision": "ff86f45dac5396341d91",
+    "url": "/react-monsters/static/js/2.ad935a5a.chunk.js"
+  },
+  {
+    "revision": "09e4f2bab13c6d71412f",
+    "url": "/react-monsters/static/js/main.bd675ffe.chunk.js"
+  },
+  {
+    "revision": "8c0c23d1a65f359e4c7a",
+    "url": "/react-monsters/static/js/runtime-main.f498133f.js"
+  },
+  {
+    "revision": "5213e2976bb75b0a29dabf5faa70c508",
+    "url": "/react-monsters/static/media/img-10.5213e297.JPG"
+  }
+]);
